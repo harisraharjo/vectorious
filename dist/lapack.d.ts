a9e57dc1ecd57418af5717632e92de7137b5bedb
harisraharjo <haris.r.putro@gmail.com>
2023-09-06 13:05:49 +0700

    update lockfile

--- dist/lapack.d.ts
+++
@@ -1,4 +0,0 @@
-import { TypedArray, DType } from './types';
-export declare function getrf(dtype: DType, m: number, n: number, x: TypedArray, ldx: number, ipiv: Int32Array): any;
-export declare function getri(dtype: DType, n: number, x: TypedArray, ldx: number, ipiv: Int32Array): any;
-export declare function gesv(dtype: DType, n: number, nrhs: number, x: TypedArray, ldx: number, ipiv: Int32Array, y: TypedArray, ldy: number): any;
